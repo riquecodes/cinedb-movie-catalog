6bd30b25619750b014d01444a4d659fd51d18742
Henrique Riva <hbriva123@gmail.com>
2025-09-23 20:22:32 -0300

    fix: adjusta a ssl configuration

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
 
