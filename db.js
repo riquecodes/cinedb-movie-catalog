db4111c2589d8a0688acc5d7976a62dc7ee35138
Henrique Riva <hbriva123@gmail.com>
2025-09-23 19:43:20 -0300

    fix: remove and change .env variables

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ const mysql = require("mysql2/promise");
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'cinema_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true
   }
 });
 
